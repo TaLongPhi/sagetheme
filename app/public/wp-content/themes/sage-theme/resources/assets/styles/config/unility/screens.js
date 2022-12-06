6b83f75e68cfbe7e841cab4f86f53d64632598d9
TaLongPhi <91329809+TaLongPhi@users.noreply.github.com>
2022-12-06 10:19:19 +0700

    update

---
+++ app/public/wp-content/themes/sage-theme/resources/assets/styles/config/unility/screens.js
@@ -0,0 +1,14 @@
+const Screens = {
+  'md': '302px',
+  'lg': '1024px',
+  'xl': '1200px',
+  '2xl': '1440px',
+  '2k': '2000px',
+  'down_xl': {'max': '1199px'},
+  'down_lg': {'max': '991px'},
+  'down_md': {'max': '767px'},
+  'down_sm': {'max': '480px'}
+}
+module.exports = {
+  Screens
+}
---
+++ app/public/wp-content/themes/sage-theme/resources/views/template-parts/page/module-test.blade.php
@@ -0,0 +1,70 @@
+<section class="mod-twocol w-full flex justify-center">
+    <div class="flex justify-center xl:flex-row w-4/5  lg:flex-col md:flex-col">
+        <div class="xl:w-2/5 flex-row mr-20 lg:w-full md:w-full">
+            <div class="line-4 w-[186px] h-[2px] bg-red-600 mb-10"></div>
+            <h2 class="w-4/5 text-[36px] lg:w-full sm:w-full">
+                The Encompass Platform Seamlessly Connects the Supply
+                Chain, Allowing You to:
+            </h2>
+        </div>
+        <div class="flex xl:w-3/5 xl:flex-col  mt-[7px] w-full lg:flex-col md:w-full md:flex-col">
+            <div class="flex xl:flex-row w-full lg:w-full lg:flex-row md:flex-col md:w-full">
+                <div class="w-1/2 mr-20 md:w-full">
+                    <h3 class="text-[24px]">
+                        Drive Business Growth
+                    </h3>
+                    <p  class="font-[200] text-[18px]">
+                    Tap into the power of predictive analytics and demand 
+                    forecasting. Capitalize on revenue-generating 
+                    opportunities faster.  Capture mission-critical data 
+                    across your entire operation—including all three supply 
+                    chain tiers—empowering you to bring your beverage or 
+                    food business into new levels of profitability. 
+                    </p>
+                </div>
+                <div class="w-1/2 md:w-full">
+                    <h3 class="text-[24px]">
+                        Leverage Real-Time Data for Better
+                        Decision-Making
+                    </h3>
+                    <p class="font-[200] text-[18px]">
+                        Accelerate business decisions with better, more
+                        accurate system-to-system data and comprehensive
+                        reporting that results in deep, actionable
+                        insights. Stay ahead of potential disruptions
+                        and maintain a finger on the pulse of your
+                        supply chain from anywhere in the world.
+                    </p>
+                </div>
+            </div>
+            <div class="flex xl:flex-row w-full lg:w-full lg:flex-row md:flex-col md:w-full">
+                <div class="w-1/2 mr-20 md:w-full">
+                    <h3 class="text-[24px]">
+                        Improve Operational efficiencies
+                    </h3>
+                    <p class="font-[200] text-[18px] ">
+                        Streamline processes and save money by reducing
+                        leakage with a fully integrated ERP system that
+                        works for your business. Encompass ERP makes it
+                        possible to identify where, when, and how
+                        resources can be best allocated to provide the
+                        most optimal outcome.
+                    </p>
+                </div>
+                <div class="w-1/2 md:w-full">
+                    <h3 class="text-[24px]">
+                        Enhance Team Dynamics & Employee
+                    </h3>
+                    <p class="font-[200] text-[18px]">
+                        When your labor force has the tools they need to
+                        succeed, the workplace runs like a well-oiled
+                        machine. With Encompass ERP, every stakeholder
+                        across your supply chain—from purchasing to
+                        Point-of-Sale—has what they need to do their
+                        jobs effectively.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
